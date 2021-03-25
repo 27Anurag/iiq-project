2547f992f0080454d3de1c6cae156bdb5c456672
GautamV234 <gautamvashishtha23@gmail.com>
2021-03-25 16:53:36 +0530

    Running Sentiment Analysis

--- audio-input/nlp.py
+++ audio-input/nlp.py
@@ -1,13 +1,11 @@
-
 # PRE INSTALLS
 # pip install SpeechRecognition
 # pip install pipwin
 # pipwin install pyaudio
 # pip install googletrans
 # pip install geopy
-
-
 # import geocoder
+
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from geopy.geocoders import Nominatim
 import reverse_geocoder as rg
@@ -19,131 +17,14 @@ from googletrans import Translator
 import geocoder
 from math import radians, cos, sin, asin, sqrt
 
+
+
+# --> GPS ke basis pe langauges --> un sab langauge pe translate --> sentiment 
+
+
 # # cuss = open("sensetive.txt", "r")
 
-# coordinate_obj = geocoder.ipinfo('me')
-# my_coords = coordinate_obj.latlng	#current coord
 
-# def to_radians(theta):
-# 	return theta * (math.pi/180)
-
-# #returns (lat, lon) at a dist r, at angle theta
-# def displace(r, theta):		# args --> (in km, in degrees)
-# 	R = 6378.1 #Radius of the Earth in km
-# 	brng = to_radians(theta) #Bearing is 90 degrees converted to radians.
-# 	d = r #Distance in km
-
-# 	#lat2  52.20444 - the lat result I'm hoping for
-# 	#lon2  0.36056 - the long result I'm hoping for.
-
-# 	lat1 = math.radians(my_coords[0]) #Current lat point converted to radians
-# 	lon1 = math.radians(my_coords[1]) #Current long point converted to radians
-
-# 	lat2 = math.asin( math.sin(lat1)*math.cos(d/R) +
-# 		math.cos(lat1)*math.sin(d/R)*math.cos(brng))
-
-# 	lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),
-# 				math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
-
-# 	lat2 = math.degrees(lat2)
-# 	lon2 = math.degrees(lon2)
-
-# 	# print(lat2)
-# 	# print(lon2)
-
-# 	return (lat2, lon2)
-
-
-# # Coordinates tuple.Can contain more than one pair.
-# coordinates = (28.613939, 77.209023)
-# # reverseGeocode(coordinates)
-
-# def reverseGeocode(coordinates): 	# arg --> ((lat, lng), (lat, lng) ... )
-# 	geolocator = Nominatim(user_agent="geoapiExercises")
-# 	Latitude = str(coordinates[0])
-# 	Longitude = str(coordinates[1])
-# 	location = geolocator.reverse(Latitude+","+Longitude)
-# 	address = location.raw['address']
-# 	# print(location)
-# 	try :
-# 		return address['state']
-# 	except:
-# 		return "none"
-
-
-# states = [	# (coord, [lang])
-# 	((11.1271, 78.6569),["Tamil","English","Hindi"]),
-# 	((19.0760, 72.8777),["English","Hindi","Marathi"])
-# ]
-
-
-# # { state : [languages] }
-
-# stateLanguagePair = {'Maharashtra':['Marathi','Hindi','English'],'Karanataka':['Kannada','English']}
-
-
-# deg=0
-# radius = 10
-# delta_theta = 6
-# allStatesFound = []
-# while(deg<360):
-# 	returned = displace(radius,deg)
-# 	foundLat = returned[0]   #in degrees
-# 	foundLong = returned[1]   #in degrees
-# 	stateFound = reverseGeocode((foundLat,foundLong))
-# 	# print("State is " + stateFound)
-# 	if stateFound not in allStatesFound:
-# 		allStatesFound.append(stateFound)
-
-# 	deg = deg + delta_theta # 360/delta_theta samples
-# 	# --> allStatesFound =  ['Mah','Gujarat','Karantaka']
-
-# languagesInState = ["Hindi", "English"]
-# for state in allStatesFound:
-# 	if state in stateLanguagePair:
-# 		# stateLanguagePair = {'Maharashtra':['Marathi','Hindi','English'],'Karanataka':['Kannada','English']}
-# 		for language in stateLanguagePair[state]:
-# 			if language!="English" and language!="Hindi":
-# 				languagesInState.append(language)
-# 				# languagesInState+=[language]
-# 	else:
-# 		pass
-# 		# should be because of mismatch of naming convention of states in both places
-
-# # languageInState should contain all the languages spoken in states in the vicinity of the area
-# print(languagesInState)
-
-# geolocator = Nominatim(user_agent="geoapiExercises")
-# Latitude = "25.594095"
-# Longitude = "85.137566"
-
-# location = geolocator.reverse(Latitude+","+Longitude)
-# print(location)
-
-# geodesic(kolkata, delhi).km
-
-
-# Question - What if the driver is from some other state
-# and the state in which he is loading/unloading is some other state?
-#
-# stateLanguagePair = {'Maharashtra':['Eng','Hin','Marathi','Gujrati']}
-
-
-# TO DO
-# Detect language based on location (GPS)  (DONE)
-# TALK TO PRAKHAR SIR ABOUT THE SPHERICAL LANGUAGE FINDING ALGORITHM  (DONE)
-
-
-####################################################################################################
-
-
-# TO DO  14/3/21
-# add code for making takeCommandHindi more generic based on diff languages identified  (DONE)
-
-translator = Translator()
-# print(googletrans.LANGUAGES)
-
-# new code added on 14/03/21 👇👇
 
 languageCodeDict = {'af': 'afrikaans',
                     'sq': 'albanian',
@@ -253,7 +134,9 @@ languageCodeDict = {'af': 'afrikaans',
                     'yo': 'yoruba',
                     'zu': 'zulu', }
 
-					
+coordinate_obj = geocoder.ipinfo('me')
+my_coords = coordinate_obj.latlng	#current coord
+
 def getCodeForLanguage(inputLanguage):
     for languageCode, language in languageCodeDict.items():
          if language == inputLanguage:
@@ -261,20 +144,54 @@ def getCodeForLanguage(inputLanguage):
  
     return "Language not translable"
 
+def to_radians(theta):
+	return theta * (math.pi/180)
 
-# new code added on 14/03/21 👇👇
+#returns (lat, lon) at a dist r, at angle theta
+def displace(r, theta):		# args --> (in km, in degrees)
+	R = 6378.1 #Radius of the Earth in km
+	brng = to_radians(theta) #Bearing is 90 degrees converted to radians.
+	d = r #Distance in km
 
+	#lat2  52.20444 - the lat result I'm hoping for
+	#lon2  0.36056 - the long result I'm hoping for.
+
+	lat1 = math.radians(my_coords[0]) #Current lat point converted to radians
+	lon1 = math.radians(my_coords[1]) #Current long point converted to radians
+
+	lat2 = math.asin( math.sin(lat1)*math.cos(d/R) +
+		math.cos(lat1)*math.sin(d/R)*math.cos(brng))
+
+	lon2 = lon1 + math.atan2(math.sin(brng)*math.sin(d/R)*math.cos(lat1),
+				math.cos(d/R)-math.sin(lat1)*math.sin(lat2))
+
+	lat2 = math.degrees(lat2)
+	lon2 = math.degrees(lon2)
+
+	# print(lat2)
+	# print(lon2)
+
+	return (lat2, lon2)
+
+
+# Coordinates tuple.Can contain more than one pair.
+coordinates = (28.613939, 77.209023)
+# reverseGeocode(coordinates)
+
+
+#translate
 def takeCommandInLanguageIdentified(lanuage):
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print('Listening')
-    r.pause_threshold = 0.7
-    audio = r.listen(source)
+        r.pause_threshold = 0.7
+        audio = r.listen(source)
     try:
         print("Recognizing")
         langaugeCode = getCodeForLanguage(lanuage)
+        print("langauage code recieved for the language is "+ str(langaugeCode))
         Query = r.recognize_google(audio, language=langaugeCode)
-        # print("the statement was='", Query, "'")
+        print("the statement was='", Query, "'")
     except Exception as e:
         print(e)
         print("Repeat the statement")
@@ -282,70 +199,181 @@ def takeCommandInLanguageIdentified(lanuage):
     return Query
 
 
-# def takeCommandHindi():
-
-#     r = sr.Recognizer()
-#     with sr.Microphone() as source:
-#         print('Listening')
-#         r.pause_threshold = 0.7
-#         audio = r.listen(source)
-#         try:
-#             print("Recognizing")
-#             Query = r.recognize_google(audio, language='hi-In')
-#             # print("the statement was='", Query, "'")
-#         except Exception as e:
-#             print(e)
-#             print("Repeat the statement")
-#             return "None"
-#         return Query
 
 
-Query = takeCommandInLanguageIdentified('hindi')  # IMPORTANT - Here hindi needs to be changed 
-												  # by the language identified based on the region 
-# print(Query)
-
-
-#  Not changed this part 👇👇
-
-
-# THIS LINE GIVES SOME KIND OF ERROR
-t = translator.translate(Query)   # I have not fixed this lets do it together
-# print(t)
-
-
-
-flg = 0
-for cusses in cuss:
-    if(cusses == t):
-        flg = 1
-        break
-
-print(("Contains profanity\n", "Does not contain profanity\n")[flg])
-
-
-# $$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%
-# DONE ON 14/03/21
-
-# TO DO 14/03/21
-# Real time Sentiment Analysis of English converted text recieved
-
-
-
-# pip install vaderSentiment
+def reverseGeocode(coordinates): 	# arg --> ((lat, lng), (lat, lng) ... )
+	geolocator = Nominatim(user_agent="geoapiExercises")
+	Latitude = str(coordinates[0])
+	Longitude = str(coordinates[1])
+	location = geolocator.reverse(Latitude+","+Longitude)
+	address = location.raw['address']
+	# print(location)
+	try :
+		return address['state']
+	except:
+		return "none"
 
 
 analyser = SentimentIntensityAnalyzer()
 
 
-# function returns 1,-1,0 for pos,neg,neutral sentiments
+# # function returns 1,-1,0 for pos,neg,neutral sentiments
 def sentimentAnalysisScores(sentence):
+    # print("Sentence recieved by sentimenetAnalysisScore is "+str(sentence))
+    sentence = str(sentence)
     SA_score = analyser.polarity_scores(sentence)
-    compundScore = SA_score['compound']
+    compundScore = SA_score['compound'] 
     if compundScore >= 0.05:
         return 1
-    elif compundScore > 0.05 and compundScore < 0.05:
+    elif compundScore > -0.05 and compundScore < 0.05:
         return 0
     else:
         return -1
 
-#
+emotion = ["neutral", "positive", "negative"]
+
+# states = [	# (coord, [lang])
+	# ((11.1271, 78.6569),["Tamil","English","Hindi"]),
+	# ((19.0760, 72.8777),["English","Hindi","Marathi"])
+# ]
+
+
+# { state : [languages] }
+
+stateLanguagePair = {'Maharashtra':['marathi','hindi','english'],'Karanataka':['kannada','english']}
+
+
+# deg=0
+# radius = 10
+# delta_theta = 6
+# allStatesFound = []
+# while(deg<360):
+# 	returned = displace(radius,deg)
+# 	foundLat = returned[0]   #in degrees
+# 	foundLong = returned[1]   #in degrees
+# 	stateFound = reverseGeocode((foundLat,foundLong))
+# 	# print("State is " + stateFound)
+# 	if stateFound not in allStatesFound:
+# 		allStatesFound.append(stateFound)
+
+# 	deg = deg + delta_theta # 360/delta_theta samples
+# 	# --> allStatesFound =  ['Mah','Gujarat','Karantaka']
+
+languagesInState = ["hindi", "english"]
+# for state in allStatesFound:
+# 	if state in stateLanguagePair:
+# 		# stateLanguagePair = {'Maharashtra':['Marathi','Hindi','English'],'Karanataka':['Kannada','English']}
+# 		for language in stateLanguagePair[state]:
+# 			if language!="english" and language!="hindi":
+# 				languagesInState.append(language)
+# 				# languagesInState+=[language]
+# 	else:
+# 		pass
+# 		# should be because of mismatch of naming convention of states in both places
+
+# languageInState should contain all the languages spoken in states in the vicinity of the area
+print(languagesInState)
+
+# # TO DO 
+#  Multi thread for running scripts for all languages side by side on jetson
+# Right Now , Run the translation only on the first Language
+
+
+# lanuageCodeRecieved = getCodeForLanguage(languagesInState[0])   # change for all languages later
+
+translator = Translator()
+print("TESTESTESTESTETS"+ str(languagesInState[0]))
+recognized_sentence=  takeCommandInLanguageIdentified(languagesInState[0])
+print("recognized sentences is "+ recognized_sentence)
+if(recognized_sentence.find('*')!=-1):
+    print("emotion is negative")
+else:
+    convertedEnglishSentence = translator.translate(recognized_sentence)
+    print("Converted to English Sentence is "+str(convertedEnglishSentence))
+    sentimentScore = sentimentAnalysisScores(convertedEnglishSentence)
+    # if(sentimentScore ==)
+    print("emotion is "+ emotion[sentimentScore])
+
+# # TO DO
+# # Change the threshold value for Positive/Negative Sentiment 
+# import soundfile as sf
+# import pyloudnorm as pyln
+
+#data, rate = sf.read(".test.wav")
+#meter = pyln.Meter(rate) #
+#loudness = meter.integrated_loudness(data)
+
+
+# # geolocator = Nominatim(user_agent="geoapiExercises")
+# # Latitude = "25.594095"
+# # Longitude = "85.137566"
+
+# # location = geolocator.reverse(Latitude+","+Longitude)
+# # print(location)
+
+# # geodesic(kolkata, delhi).km
+# # Question - What if the driver is from some other state
+# # and the state in which he is loading/unloading is some other state?
+
+# stateLanguagePair = {'Maharashtra':['Eng','Hin','Marathi','Gujrati']}
+# # TO DO
+# # Detect language based on location (GPS)  (DONE)
+# # TALK TO PRAKHAR SIR ABOUT THE SPHERICAL LANGUAGE FINDING ALGORITHM  (DONE)
+# ####################################################################################################
+# # TO DO  14/3/21
+# # add code for making takeCommandHindi more generic based on diff languages identified  (DONE)
+# translator = Translator()
+# # print(googletrans.LANGUAGES)
+# # new code added on 14/03/21 👇👇
+# # new code added on 14/03/21 👇👇
+# # def takeCommandHindi():
+# #     r = sr.Recognizer()
+# #     with sr.Microphone() as source:
+# #         print('Listening')
+# #         r.pause_threshold = 0.7
+# #         audio = r.listen(source)
+# #         try:
+# #             print("Recognizing")
+# #             Query = r.recognize_google(audio, language='hi-In')
+# #             # print("the statement was='", Query, "'")
+# #         except Exception as e:
+# #             print(e)
+# #             print("Repeat the statement")
+# #             return "None"
+# #         return Query
+
+
+# Query = takeCommandInLanguageIdentified('hindi')  # IMPORTANT - Here hindi needs to be changed 
+# 												  # by the language identified based on the region 
+# # print(Query)
+
+
+# #  Not changed this part 👇👇
+
+
+# # THIS LINE GIVES SOME KIND OF ERROR
+# t = translator.translate(Query)   # I have not fixed this lets do it together
+# # print(t)
+
+
+
+# flg = 0
+# for cusses in cuss:
+#     if(cusses == t):
+#         flg = 1
+#         break
+
+# print(("Contains profanity\n", "Does not contain profanity\n")[flg])
+
+
+# # $$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%####$$$$$%%%%%%
+# # DONE ON 14/03/21
+
+# # TO DO 14/03/21
+# # Real time Sentiment Analysis of English converted text recieved
+
+
+
+# # pip install vaderSentiment
+
+
